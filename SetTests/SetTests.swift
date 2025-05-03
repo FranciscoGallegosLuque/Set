84fc352a72f4242ae642009be2c92b2b8d35f274
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-05-03 19:54:08 -0300

    Initial Commit

---
+++ SetTests/SetTests.swift
@@ -0,0 +1,17 @@
+//
+//  SetTests.swift
+//  SetTests
+//
+//  Created by Francisco Manuel Gallegos Luque on 03/05/2025.
+//
+
+import Testing
+@testable import Set
+
+struct SetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
