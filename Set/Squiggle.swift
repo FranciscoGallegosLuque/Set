4d05a1814f6380349bde4846ffa86bb4cdfb1d25
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-07-07 16:58:00 -0300

    [Feature] Added Squiggle shape and made general logic improvements.

---
+++ Set/Squiggle.swift
@@ -0,0 +1,18 @@
+//
+//  Squiggle.swift
+//  Set
+//
+//  Created by Francisco Manuel Gallegos Luque on 07/07/2025.
+//
+
+import SwiftUI
+
+struct Squiggle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Squiggle()
+}
