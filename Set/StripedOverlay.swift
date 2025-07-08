bba717d71eb07bec5a0c1a7338d3f02bb7be32eb
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-07-08 14:14:39 -0300

    [Feature]  Added striped shading

---
+++ Set/StripedOverlay.swift
@@ -0,0 +1,18 @@
+//
+//  StripedOverlay.swift
+//  Set
+//
+//  Created by Francisco Manuel Gallegos Luque on 08/07/2025.
+//
+
+import SwiftUI
+
+struct StripedOverlay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StripedOverlay()
+}
